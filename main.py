6b5635e5b20c4b89eec9cd76e50e50a6170b433c
rikj26 <54986396+rikj26@users.noreply.github.com>
2024-06-11 20:03:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from classes import station, verbinding
+from algorithms import random
+import visualisatie
 
 if __name__ == "__main__":
     station_test = station.laad_stations("data/StationsHolland.csv")
     verbinding_test = verbinding.laad_verbindingen("data/ConnectiesHolland.csv")
 
-    visualisatie.visualisatie(station_test, "data/kaarten/netherlands_.geojson")
+    random_algoritme = random.random_traject(station_test, verbinding_test)
+    for station in random_algoritme:
+        print(station.name)
+
+    visualisatie.visualisatie(random_algoritme, "data/kaarten/netherlands_.geojson")
