c2169891231a7650b4b267453344a3f6f3df6692
rikj26 <54986396+rikj26@users.noreply.github.com>
2024-06-11 17:20:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     station_test = station.laad_stations("data/StationsHolland.csv")
     verbinding_test = verbinding.laad_verbindingen("data/ConnectiesHolland.csv")
 
-    for i in verbinding_test:
-        print(i.station1, i.station2)
+    visualisatie.visualisatie(station_test, "data/kaarten/netherlands_.geojson")
