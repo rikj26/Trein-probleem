6a0f81361d5c6ec9c92e898d34d4498c2f67d79c
rikj26 <54986396+rikj26@users.noreply.github.com>
2024-06-13 19:57:34 +0200

    Update traject.py

--- classes/traject.py
+++ classes/traject.py
@@ -1,19 +1,10 @@
-class Traject:
+class Traject():
     def __init__(self):
-        self.verbindingen = []
-        self.tijd = 0
-
-    def verbinding_toevoegen(self, verbinding):
-        self.verbindingen.append(verbinding)
-        self.tijd += verbinding.time
-
-    def totale_tijd(self):
-        return self.tijd
-
-    def stations(self):
-        station_lijst = [self.verbindingen[0].station1]
+        self.route = []
+        self.total_time = 0
+    
+    def add_station(self, station):
+        if len(self.route) > 1:
+            self.total_time += self.route[-1].connecties[station.name]
         
-        for verbinding in self.verbindingen:
-            station_lijst.append(verbinding.station2)
-        
-        return station_lijst
+        self.route.append(station)
