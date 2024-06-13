4fb8f5c19d88b290408a1ea166c4d7fe86866838
rikj26 <54986396+rikj26@users.noreply.github.com>
2024-06-13 21:07:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 from classes import station, traject
 from algorithms import random
 import visualisatie
@@ -6,10 +8,19 @@ if __name__ == "__main__":
     stations_holland = station.laad_stations("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
     stations_nationaal = station.laad_stations("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv")
 
-    random_algoritme = random.random_traject(stations_nationaal, traject.Traject())
-    for station in random_algoritme.route:
-        print(station.name)
+    # Dit is een tijdelijke oplossing, en moet anders. Maar ik heb het idee dat ik een verkeerde representatie heb gekozen.
+    max_connecties_holland = 0
+    with open("data/ConnectiesHolland.csv", 'r') as in_file:
+        reader = csv.DictReader(in_file)
+        for row in reader:
+            max_connecties_holland += 1
 
-    print("The totale tijd is:", random_algoritme.total_time)
+    gem_K_score = 0
+    for _ in range(500):
+        random_algoritme = random.random_traject(stations_holland, traject.Traject())
+        aantal_verbindingen = len(random_algoritme.route) - 1
+        gem_K_score += aantal_verbindingen / max_connecties_holland * 10000 - ((aantal_verbindingen * 100) + random_algoritme.total_time)
+
+    print("De gemiddelde K-scores is:", gem_K_score / 500)
 
     visualisatie.visualisatie(random_algoritme.route, "data/kaarten/netherlands_.geojson")
