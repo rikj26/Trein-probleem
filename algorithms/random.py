727fb4a0e2f7de5400eedd42c36a3b3c55886977
rikj26 <54986396+rikj26@users.noreply.github.com>
2024-06-17 16:01:47 +0200

    Update random.py

--- algorithms/random.py
+++ algorithms/random.py
@@ -1,34 +1,39 @@
 import random
 
-def random_traject(stations, traject): # stations: lijst met alle objecten van stations
-    current_station = random.choice(stations)
-    traject.add_station(current_station)
-    previous_station = None
+def random_traject(network):
+    while(len(network.trajects) < 7 and len(network.connections) != 0):
+        traject = network.create_traject()
+        random_connection = random.choice(network.connections)
+        network.connections.remove(random_connection)
 
-    while(len(traject.route) < 8):
-        possible_stations = list(current_station.connecties.keys())
-
-        # Het moet niet mogelijk zijn om direct hetzelfde traject terug te nemen, behalve als dit de enige optie is.
-        if previous_station and len(possible_stations) > 1:
-            possible_stations.remove(previous_station)
-
-        next_station = random.choice(possible_stations)
-        while traject.total_time + current_station.connecties[next_station] > 120:
-            possible_stations.remove(next_station)
+        traject.add_station(random_connection.station1)
+        traject.add_station(random_connection.station2)
+        traject.update_time(random_connection.time)
+        while(traject.total_time <= 120):
+            current_station = traject.get_last_station()
+            possible_connections = get_possible_connections(current_station, network.connections)
             
-            # wanneer alle opties ervoor zorgen dat de tijd groter wordt 120, stuur dan het huidige traject
-            if not possible_stations:
-                return traject
-            
-            next_station = random.choice(possible_stations)
+            if not possible_connections:
+                break
 
-        previous_station = current_station.name
-        current_station = get_new_station(next_station, stations)
-        traject.add_station(current_station)
-    
-    return traject
+            random_connection = random.choice(possible_connections)
+            if random_connection.time + traject.total_time > 120:
+                break
 
-def get_new_station(new_station, stations): # new_station : string met stationsnaam, stations : lijst met alle objecten van stations
-    for station in stations:
-        if station.name == new_station:
-            return station
+            if current_station.name == random_connection.station1.name:
+                traject.add_station(random_connection.station2)
+            elif current_station.name == random_connection.station2.name:
+                traject.add_station(random_connection.station1)
+
+            traject.update_time(random_connection.time)
+            network.connections.remove(random_connection)
+
+        network.add_traject(traject)
+    return network.trajects
+
+def get_possible_connections(station, connections):
+    possible_connections = []
+    for connection in connections:
+        if station.name == connection.station1.name or station.name == connection.station2.name:
+            possible_connections.append(connection)
+    return possible_connections
