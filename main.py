40f2f11523a005a692bcc738f631c457c29ebc70
rikj26 <54986396+rikj26@users.noreply.github.com>
2024-06-18 15:45:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from classes import network
-from algorithms import random
+from algorithms import random, greedy
 import visualisatie
 
 if __name__ == "__main__":
     netwerk_holland = network.Network("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
     netwerk_nationaal = network.Network("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv")
     
-    random_algoritme = random.random_traject(netwerk_holland)
+    random_algoritme = random.random_traject(netwerk_nationaal, 20, 180)
 
-    visualisatie.visualisatie(random_algoritme.trajects, "data/kaarten/netherlands_.geojson")
+    greedy_algoritme = greedy.greedy_traject(netwerk_nationaal, 20, 180)
 
-    print(random_algoritme.score())
+    visualisatie.visualisatie(greedy_algoritme.trajects, "data/kaarten/netherlands_.geojson")
+
+    scores = []
+    for _ in range(10000):
+        test_random = random.random_traject(netwerk_holland, 7, 120)
+        scores.append(test_random.score())
+        test_random.clear_trajects()
+
+    visualisatie.histogram(scores, 'random algorithm')
