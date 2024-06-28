5f30abffea7f34fd5229a3c36494a81c06f04680
rikj26 <54986396+rikj26@users.noreply.github.com>
2024-06-28 02:58:48 +0200

    Update bfs.py

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -1,59 +1,57 @@
 import copy
 
-def bfs_traject(network, max_trajects, max_time):
-    original_connections = copy.deepcopy(network.connections)
+class BFS:
+    def __init__(self, network, max_trajects, max_time):
+        self.network = network
+        self.max_trajects = max_trajects
+        self.max_time = max_time
     
-    while len(network.trajects) < max_trajects and len(network.connections) != 0:
-        traject = network.create_traject()
-        
-        if not network.connections:
-            break
-        
-        start_connection = network.connections[0]
-        queue = [(start_connection.time, [start_connection.station1, start_connection.station2])]
-        visited = set()
-        visited.add((start_connection.station1.name, start_connection.station2.name))
-        visited.add((start_connection.station2.name, start_connection.station1.name))
-        network.connections.remove(start_connection)
-        
-        while queue:
-            current_time, path = queue.pop(0)
-            current_station = path[-1]
-            
-            if current_time > max_time:
-                continue
-            
-            possible_connections = get_possible_connections(current_station, network.connections)
-            
-            for connection in possible_connections:
-                if current_station.name == connection.station1.name and current_time + connection.time <= max_time:
-                    new_path = path + [connection.station2]
-                    if (connection.station1.name, connection.station2.name) not in visited:
-                        queue.append((current_time + connection.time, new_path))
-                        visited.add((connection.station1.name, connection.station2.name))
-                        visited.add((connection.station2.name, connection.station1.name))
-                        network.connections.remove(connection)
-                elif current_station.name == connection.station2.name and current_time + connection.time <= max_time:
-                    new_path = path + [connection.station1]
-                    if (connection.station2.name, connection.station1.name) not in visited:
-                        queue.append((current_time + connection.time, new_path))
-                        visited.add((connection.station2.name, connection.station1.name))
-                        visited.add((connection.station1.name, connection.station2.name))
-                        network.connections.remove(connection)
-        
-        for station in path:
+    def bfs(self):
+        for _ in range(self.max_trajects):
+
+            queue = self.initialize_queue()
+            best_traject = self.network.create_traject()
+
+            while queue:
+                current_traject = queue.pop(0)
+                current_station = current_traject.get_last_station()
+
+                for connection in self.get_possible_connections(current_station):
+
+                    if connection in current_traject.covered_connections:
+                        continue
+
+                    next_station = connection.station2 if current_station == connection.station1 else connection.station1
+                
+                    if current_traject.total_time + connection.time > self.max_time:
+                        if current_traject != best_traject and len(current_traject.route) > len(best_traject.route):
+                            best_traject = current_traject
+                    else:
+                        new_traject = copy.deepcopy(current_traject)
+                        new_traject.add_station(next_station)
+                        new_traject.update_time(connection.time)
+                        new_traject.add_covered_connection(connection)
+                        queue.append(new_traject)
+
+            self.network.trajects.append(best_traject)
+            for connection in best_traject.covered_connections:
+                self.network.connections.remove(connection)
+    
+        return self.network
+    
+    def initialize_queue(self):
+        queue = []
+
+        for station in self.network.stations.values():
+            traject = self.network.create_traject()
             traject.add_station(station)
-        traject.update_time(current_time)
+            queue.append((traject))
         
-        network.add_traject(traject)
+        return queue
     
-    network.connections = original_connections
-
-    return network
-
-def get_possible_connections(station, connections):
-    possible_connections = []
-    for connection in connections:
-        if station.name == connection.station1.name or station.name == connection.station2.name:
-            possible_connections.append(connection)
-    return possible_connections
+    def get_possible_connections(self, station):
+        possible_connections = []
+        for connection in self.network.connections:
+            if station == connection.station1 or station == connection.station2:
+                possible_connections.append(connection)
+        return possible_connections
